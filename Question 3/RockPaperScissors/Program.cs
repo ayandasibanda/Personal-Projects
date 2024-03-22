ac3779a6a66c54071aae768da16b7d05b387feca
ayandasibanda <ayanda.sibanda17@gmail.com>
2024-03-22 05:28:07 +0200

    adding files

---
+++ Question 3/RockPaperScissors/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Array of choices for the computer to choose from
+            string[] computerChoices = { "rock", "paper", "scissors"};
+
+            // Generate a random choice for computer
+            Random random = new Random();
+            int randomChoice = random.Next(0, 2); // Generates a random index between 0 and 2
+            string computerChoice = computerChoices[randomChoice];
+
+            // Prompt the user to enter their choice
+            Console.WriteLine("Please enter your choice between rock, paper, scissors by entering a character!");
+            Console.WriteLine("r = rock");
+            Console.WriteLine("p = paper");
+            Console.WriteLine("s = scissors");
+            Console.WriteLine();
+            string userChoice = Console.ReadLine().ToLower();
+
+            // Determine the winner
+            string result = DetermineWinner(userChoice, computerChoice);
+
+            // Display the result
+            Console.WriteLine($"The computer chose: {computerChoice}");
+            Console.WriteLine(result);
+
+            // Terminate program
+            Console.WriteLine("Press any key to exit the program");
+            Console.ReadKey();
+        }
+
+        // Helper method to determine the winnder
+        static string DetermineWinner(string userChoice, string computerChoice)
+        {
+            if (userChoice == computerChoice)
+            {
+                return "It is a tie!";
+            }
+            else if ((userChoice == "rock" && computerChoice == "scissors") ||
+                (userChoice == "paper" && computerChoice == "rock") ||
+                (userChoice == "scissors" && computerChoice == "paper"))
+            {
+                return "You are the winner! Congratulations, you outsmarted a machine!";
+            }
+            else
+            {
+                return "The computer wins! Looks like the computer has a PhD in Rock-Paper-Scissorology!";
+            }
+
+        }
+
+    }
+}
---
+++ Question 4/PerfectNumber/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerfectNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Iterate through number from 1 to 10,000
+            for (int number = 1; number <= 10000; number++)
+            {
+                // Check if the current number is perfect
+                if (NumberIsPerfect(number))
+                {
+                    // If the number is perfect then display it in perfect format
+                    Console.WriteLine($"{number,8} is perfect");
+                    Console.ReadKey();
+                }
+            }
+        }
+
+        // Method to check if the number is perfect
+        static bool NumberIsPerfect(int number)
+        {
+            // Initialize a variable to store the sum of the divisors
+            int sum = 0;
+
+            // Iterate through all possible divisors of the number
+            for (int i = 1; i <= number / 2; i++)
+            {
+                // If i is a divisor of the number, it gets added to the sum
+                if (number % i == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            // If the sum of the divisors equals to the number itself then the number is perfect
+            return sum == number;
+        }
+
+    }
+}
